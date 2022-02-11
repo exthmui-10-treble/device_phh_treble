a9a5414bc2099ed106b93532033374d4022de942
xiaoleGun <1592501605@qq.com>
2022-02-11 19:27:50 +0800

    treble: import ota json from gist

--- miku.mk
+++ miku.mk
@@ -7,3 +7,7 @@ TARGET_NO_KERNEL := true
 
 # Maintaier
 MIKU_MASTER := xiaoleGun
+
+# OTA json
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	ro.system.ota.json_url=https://gist.githubusercontent.com/xiaoleGun/4e8f9c8473f6a5dd1e261d6edd345a61/raw/cca987a26558b157f1811a7971e1df4ccab6f793/ota.json
