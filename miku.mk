89962d5ca061092dfa66156f85c0de88c2a42e8d
xiaoleGun <1592501605@qq.com>
2022-03-12 12:45:52 +0800

    treble: import via browser
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- miku.mk
+++ miku.mk
@@ -11,3 +11,12 @@ MIKU_MASTER := xiaoleGun
 # OTA json
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 	ro.system.ota.json_url=https://gist.githubusercontent.com/xiaoleGun/4e8f9c8473f6a5dd1e261d6edd345a61/raw/cca987a26558b157f1811a7971e1df4ccab6f793/ota.json
+
+# Browser
+ifeq ($(TARGET_PRODUCT), miku_treble_arm64_bgN)
+PRODUCT_PACKAGES += \
+    Chrome
+else
+PRODUCT_PACKAGES += \
+    Via
+endif
