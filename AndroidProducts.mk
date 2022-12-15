bbdee6f666436287becc9564cd13a73838cfa208
xiaoleGun <1592501605@qq.com>
2022-12-15 17:42:57 +0800

    treble: Import exthm product makefile
    Signed-off-by: xiaoleGun <1592501605@qq.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/exthm_treble_arm64_avN.mk \
+	$(LOCAL_DIR)/exthm_treble_arm64_bvN.mk
---
+++ exthm_treble_arm64_avN.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, vendor/vndk/vndk.mk)
+$(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+
+include build/make/target/product/aosp_arm64.mk
+
+PRODUCT_DEVICE := exthm_treble_arm64_a
+PRODUCT_MODEL := exTHmUI GSI on ARM64
+PRODUCT_NAME := exthm_treble_arm64_avN
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+
+PRODUCT_CHARACTERISTICS := device
+
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS :=
+
+# PHH "secure"
+PRODUCT_COPY_FILES += \
+    $(LOCAL_DIR)/empty:$(TARGET_COPY_OUT_SYSTEM)/etc/phh-secure
