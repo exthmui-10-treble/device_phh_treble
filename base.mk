fa9c8d8cad26ae9d200586cd64bfda076f92eb14
xiaoleGun <1592501605@qq.com>
2022-12-15 17:04:32 +0800

    treble: Clean up
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- base.mk
+++ base.mk
@@ -7,10 +7,6 @@ PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
 
-#Use a more decent APN config
-PRODUCT_COPY_FILES += \
-	device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/phh/treble/sepolicy
 DEVICE_PACKAGE_OVERLAYS += device/phh/treble/overlay
 
