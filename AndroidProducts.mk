02bf9cc7b7e10f0769b6ced04a18799fbbb45b25
xiaoleGun <1592501605@qq.com>
2022-12-19 07:43:15 +0800

    treble: Import Huawei general system product makefile
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/exthm_treble_arm64_avN.mk \
-	$(LOCAL_DIR)/exthm_treble_arm64_bvN.mk
+	$(LOCAL_DIR)/exthm_treble_arm64_bvN.mk \
+    $(LOCAL_DIR)/exthm_huawei_general_avN.mk \
+    $(LOCAL_DIR)/exthm_huawei_general_bvN.mk
---
+++ exthm_huawei_general_bvN.mk
@@ -0,0 +1,26 @@
+$(call inherit-product, vendor/vndk/vndk.mk)
+$(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
+
+include build/make/target/product/aosp_arm64_ab.mk
+
+PRODUCT_DEVICE := exthm_treble_arm64_ab
+PRODUCT_MODEL := Huawei General System
+PRODUCT_NAME := exthm_huawei_general_bvN
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+
+PRODUCT_CHARACTERISTICS := device
+
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS :=
+
+# HwCamera2
+PRODUCT_PACKAGES += \
+    HwCamera2
+
+# PHH "secure"
+PRODUCT_COPY_FILES += \
+    device/phh/treble/empty:$(TARGET_COPY_OUT_SYSTEM)/etc/phh-secure
+
+# Faceunlock
+TARGET_FACE_UNLOCK := true
